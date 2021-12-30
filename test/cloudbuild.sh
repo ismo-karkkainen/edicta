1bd67a3ffb200769213b86dd2c172ba4187ed599
ismo-karkkainen <3896372+ismo-karkkainen@users.noreply.github.com>
2021-12-30 22:09:25 +0200

    Use convenience scripts added to cloud-sdk image.

--- test/cloudbuild.sh
+++ test/cloudbuild.sh
@@ -7,7 +7,8 @@ export X=$(ruby --version | cut -d ' ' -f 2)
 
 cd $R
 (
-    echo "Build $(cat _logs/commit.txt) on $D Ruby $X at $(date '+%Y-%m-%d %H:%M')"
+    echo "Built on $D Ruby $X at $(date '+%Y-%m-%d %H:%M')"
+    cat _logs/info.txt
     rake test
     echo "Test exit code: $?"
 ) 2>&1 | tee -a "$R/_logs/$D-$X.log"
